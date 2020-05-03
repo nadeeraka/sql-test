1fbb7c43e1f0d609b8ef2f6f40e9f8fd69d20da5
nadeeraka <nadeerakan@gmail.com>
2020-05-03 16:28:30 +0530

    7

--- poster.sql
+++ poster.sql
@@ -1,15 +1,25 @@
 
 create table users(
+    id serial primary key ,
     user_name varchar(100)  not null,
     last_name varchar(100) ,
     email      varchar(255) unique not null ,
     password text
-)
--- drop table users;
-insert into  users (user_name, last_name, email, password)
-values ('bob','bobiee', 'bob@bob.com','q2w3e4^%$$$£r5t£$%6y7u8i90"£')
+);
+ drop table users;
+insert into  users (id,user_name, last_name, email, password)
+values (default,'bob','bobiee', 'bob@bob.com','q2w3e4^%$$$£r5t£$%6y7u8i90"£')
 
 select user_name from users;
+-- note this not efficient way
+select user_name, last_name, email, password from users;
 
-select user_name, last_name, email, password from users
+
+-- this is the right way
+select * from  users;
+
+-- modifying table
+
+alter table users add column age int default null
+alter table users drop column  age
 
